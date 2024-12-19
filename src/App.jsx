6707f9906bccc3d75ab7de7044b21246cf3f86f1
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-12-19 18:38:44 +0330

    Dev app codule

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import {useRoutes} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useRoutes } from "react-router-dom";
 import routes from "./routes.jsx";
+import apiClient from "./../configs/axios.jsx";
 import "./App.css";
 
 function App() {
-    const router = useRoutes(routes);
+  const [mainUser, setMainUser] = useState(window.localStorage.getItem("id"));
 
-    return (
-        <>
-            {router}
-        </>
-    )
+  useEffect(() => {
+    const locationUrl = window.location.pathname;
+
+    if (!locationUrl.includes("/auth/")) {
+      if (!mainUser) {
+        window.localStorage.clear();
+        window.location.href = "/auth/login";
+      }
+
+      const verifyAccessToken = async () => {
+        try {
+          await apiClient.put("/auth/refresh");
+        } catch (err) {
+          document.cookie = "";
+          window.localStorage.clear();
+          window.location.href = "/auth/login";
+        }
+      };
+
+      verifyAccessToken();
+    }
+  }, []);
+
+  const router = useRoutes(routes);
+
+  return <>{router}</>;
 }
 
 export default App;
